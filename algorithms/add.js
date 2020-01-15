59acde283242273c78bf04c6039737d9e8f177ed
Jose Cuevas <josecueram@gmail.com>
2020-01-14 18:14:13 -0600

    Added instructions to add algorithm

--- algorithms/add.js
+++ algorithms/add.js
@@ -1,1 +1,4 @@
+// TODO: Write a function that returns the sum of two numbers.
+// * For param1 = 1 and param2 = 2, the output should be add(param1, param2) = 3
+
 const add = (param1, param2) => param1 + param2
